4d003e810f6ea626eabc84563154fca808bc0edf
aswanyshaji <125889357+aswanyshaji@users.noreply.github.com>
2023-04-05 20:19:42 +0530

    Add files via upload

--- co2_gdp_health.py
+++ co2_gdp_health.py
@@ -8,29 +8,52 @@ Created on Mon Apr  3 17:22:17 2023
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def read_transpose_clean(filename):
+def read_clean_transpose(filename):
     data_frame = pd.read_excel(filename)
-    # Set the Index on Country Name - previously was numeric.
-    data_frame_transpose = data_frame.set_index('Country Name')
-    # Transpose the index and columns
-    data_frame_transpose = data_frame_transpose.transpose()
-    # Reset the index, sets, 'index' as a column
-    data_frame_transpose = data_frame_transpose.reset_index()
-    # Rename the 'index' column to year
-    data_frame_transpose = data_frame_transpose.rename(columns={'index':'Year'})
-    # Set the DataFrame Index to newly named 'Year'
-    data_frame_transpose = data_frame_transpose.set_index('Year')
-    return data_frame, data_frame_transpose
+    data_frame.iloc[3:]
+    data_frame.set_index('Country Name', inplace =True)
+    data_frame.drop(labels = ['Country Code', 'Indicator Name', 'Indicator Code'], axis = 1, inplace = True)
+    data_frame.dropna(axis=0,how='all',thresh=None, subset=None, inplace=True)
+    data_frame.dropna(axis=1,how='all',thresh=None, subset=None, inplace=True)
+    clean_df_transpose = data_frame.transpose()
+    #clean_df_transpose.rename(columns={'index':'Year'}, inplace = True)
+    clean_df_transpose = clean_df_transpose.rename_axis('Year')
+    return data_frame, clean_df_transpose
 
-def clean_data(df):
-    df.drop(labels = ['Country Code', 'Indicator Name', 'Indicator Code'], axis = 0, inplace = True)
-    df.dropna(axis=0,how='all',thresh=None, subset=None, inplace=True)
-    df.dropna(axis=1,how='all',thresh=None, subset=None, inplace=True)
-    return df
-
-if __name__ == "__main__" :
+def co2_emission_trend(df):
+    top_five = df.loc[['China', 'United States', 'India', 'Japan', 'Germany']]
+    top_five_year =top_five.loc[:, ["2005", "2010", "2015", "2019"]]
+    print(top_five_year)
+    top_five_year.plot(kind ='bar',)
+    plt.title("CO2 EMISSION TREND OF TOP 5 COUNTRIES")
+    plt.ylabel("CO2 EMISSION (Kilo tonnes)")
+    plt.xlabel("COUNTRY")
     
-    df1, df1_t = read_transpose_clean('co2_emission.xls')
-    clean_df = clean_data(df1_t)
-    clean_df.to_excel('clean2.xlsx')
+def gdp_growth_trend(df):
+    top_five = df.loc[['China', 'United States', 'India', 'Japan', 'Germany']]
+    top_five_year =top_five.loc[:, ["2005", "2010", "2015", "2019"]]
+    print(top_five_year)
+    top_five_year.plot(kind ='bar',)
+    plt.title("GDP GROWTH TREND OF TOP CARBON EMITTING COUNTRIES")
+    plt.ylabel("GDP GROWTH")
+    plt.xlabel("COUNTRY")    
+    
+    
+
+
+if __name__ == "__main__":
+
+    co_2, co2_trans = read_clean_transpose('co2.xls')
+    print(co_2)
+    print(co2_trans)
+   # co2_trans.to_excel("trans2.xlsx")
+    co2_emission_trend(co_2)
+    gdp_growth, gdp_growth_trans = read_clean_transpose('gdp.xls')
+    gdp_growth_trend(gdp_growth)
+    
+    
+    
+    
+    
+    
     
